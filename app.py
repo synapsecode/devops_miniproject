414d05f3496c1d237cb2bc550bbace430a8e6802
Manas Hejmadi <manas2all@outlook.com>
2025-05-26 05:14:58 +0530

    Added Run Instructions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>MANAS, MANISH, NANDINI PROJECT</h1>"
+    return "<h1>MANAS, MANISH, NANDINI PROJECT v2</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
