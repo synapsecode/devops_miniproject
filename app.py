259aae8d633e495b45f068dabd6a870377a5f8c2
Manas Hejmadi <manas2all@outlook.com>
2025-05-26 10:07:01 +0530

    Basic Frontend Added

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>MANAS, MANISH, NANDINI PROJECT v2</h1>"
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    return send_from_directory(current_directory, 'app.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
